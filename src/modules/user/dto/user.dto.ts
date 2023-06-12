4c8904734830598097859a9ab04a54e501c4cbd8
MarcosPauloV <marcospvs96@gmail.com>
2023-06-12 15:45:56 -0300

    📦️ chore(user.dto.ts): change interface to type to keep semantics

---
+++ src/modules/user/dto/user.dto.ts
@@ -0,0 +1,15 @@
+export type UserDTO = {
+	id?: string
+	CPF: string
+	email: string
+	name: string
+	lastName: string
+	password: string
+	sex: string
+	phone: string
+	birthdate: string
+	CEP: string
+	number: string
+	city: string
+	UF: string
+}
